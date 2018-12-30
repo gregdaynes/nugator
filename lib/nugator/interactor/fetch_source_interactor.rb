f4bcd6798fccd599e26dbefbc915768d0382572e
Gregory Daynes <gregdaynes@gmail.com>
2018-12-29 17:54:31 -0800

    rename fetch source interactor

--- lib/nugator/interactor/fetch_source_interactor.rb
+++ lib/nugator/interactor/fetch_source_interactor.rb
@@ -4,15 +4,13 @@ module Nugator
       include Nugator::NUGATOR_CONTAINER[
         'operation.source.fetch',
         'operation.source.parse_item',
-        repository: 'repository.items'
       ]
 
       def call(source)
         results = fetch.call(source.url)
 
-        results.each do |raw_item|
-          parsed_item = parse_item.call(raw_item)
-          repository.create(parsed_item)
+        results.map do |raw_item|
+          parse_item.call(raw_item)
         end
       end
     end
