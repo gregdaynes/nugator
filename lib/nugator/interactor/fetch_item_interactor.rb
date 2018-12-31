e8918202115e19574941a05fb91e5af7220f03ca
Gregory Daynes <gregdaynes@gmail.com>
2018-12-30 17:37:56 -0800

    remove extracted operations from fetch_item_interactor

--- lib/nugator/interactor/fetch_item_interactor.rb
+++ lib/nugator/interactor/fetch_item_interactor.rb
@@ -2,78 +2,18 @@ module Nugator
   module Interactor
     class FetchItem
       include Nugator::NUGATOR_CONTAINER[
+        'operation.item.validate_request',
+        'operation.item.fetch_item',
+        'operation.item.filter_content',
         repository: 'repository.item'
       ]
 
       def call(**options)
-        @ids    = options[:id]
-        @count  = options[:count]
-        @before = options[:before]
-        @since  = options[:since]
-        @tags   = options[:tags]
-        @show_content = options[:content]
+        validate_request.call(**options)
+        items = fetch_item.call(**options) || []
+        items = filter_content.call(items) unless options[:content]
 
-        validate_request
-
-        handle_reversed_date_range
-
-        items = fetch_items || []
-
-        filter_content(items)
-      end
-
-      private
-
-      attr_reader :ids, :count, :show_content, :before, :since, :tags
-
-      def validate_request
-        validate_ids_request
-        validate_count_request
-      end
-
-      def validate_ids_request
-        raise Nugator::Error::InvalidRequest if ids && (tags  ||
-                                                        count ||
-                                                        since ||
-                                                        before)
-      end
-
-      def validate_count_request
-        raise Nugator::Error::InvalidRequest if count && (count <= 0 ||
-                                                          count > 10)
-      end
-
-      def fetch_items
-        if ids
-          items_by_ids
-        else
-          items_by_combinator
-        end
-      end
-
-      def items_by_ids
-        repository.get_items_by_ids(ids)
-      end
-
-      def items_by_combinator
-        repository.get_items_by_combinator(since: since,
-                                           before: before,
-                                           count: count,
-                                           tags: tags)
-      end
-
-      def handle_reversed_date_range
-        return unless since && before
-
-        @since, @before = @before, @since if since > before
-      end
-
-      def filter_content(items)
-        return items if show_content
-
-        items.each do |item|
-          item.delete_field(:content)
-        end
+        items
       end
     end
   end
--- spec/lib/nugator/interactor/fetch_item_interactor_spec.rb
+++ spec/lib/nugator/interactor/fetch_item_interactor_spec.rb
@@ -6,14 +6,15 @@ require 'dry/container/stub'
 require 'nugator'
 
 RSpec.describe Nugator::Interactor::FetchItem do
-  let(:item_repository) { Persistance::Item.new }
+  let!(:item_repository) { Persistance::Item.new }
 
   let(:instance) { described_class.new }
-  let(:id) { [1] }
+
+  let(:content) { nil }
   let(:request) do
     {
-      id: id,
-      content: false,
+      id: nil,
+      content: content,
       count: nil,
       before: nil,
       since: nil
@@ -23,259 +24,29 @@ RSpec.describe Nugator::Interactor::FetchItem do
   before do
     Nugator::Container.enable_stubs!
     Nugator::Container.stub('repository.item', item_repository)
+
+    # validate_request is not under test
+    allow_any_instance_of(Nugator::Operation::Item::ValidateRequest)
+      .to receive(:call) { nil }
+
+    # fetch_item is not under test
+    allow_any_instance_of(Nugator::Operation::Item::FetchItem)
+      .to receive(:call) { [OpenStruct.new(item_repository.data[1])] }
   end
 
   subject { instance.call(request) }
 
-  context 'when request for items by id' do
-    context 'with single id' do
-      it 'returns an item by id' do
-        item = subject.first
-
-        expect(subject).to be_a Array
-        expect(subject.count).to be 1
-
-        expect(item.id).to be 1
-        expect(item.author).to eq 'Testy McTesterson'
-        expect(item.title).to eq 'Test Title 1'
-        expect(item.link).to eq 'https://example.com'
-        expect(item.excerpt).to eq 'Test Excerpt'
-        expect(item.content).to be nil
-      end
-
-      it 'returns an full item' do
-        request[:id] = [2]
-        request[:content] = true
-
-        item = subject.first
-
-        expect(item.id).to be 2
-        expect(item.content).to eq 'Test Content'
-      end
-    end
-
-    context 'when request for items by id' do
-      it 'returns items' do
-        request[:id] = [1, 2]
-
-        items = subject
-
-        expect(items.first.id).to eq(1 || 2)
-        expect(items.count).to eq 2
-      end
-
-      it 'returns full items' do
-        request[:id] = [1, 2]
-        request[:content] = true
-
-        items = subject
-
-        expect(items.first.content).to eq 'Test Content'
-        expect(items.last.content).to eq 'Test Content'
-      end
-
-      it 'returns found items only' do
-        request[:id] = [1, 2, 666]
-
-        expect(subject.count).to eq 2
-      end
-    end
-
-    context 'with invalid params' do
-      it 'fails when cannot find item by id' do
-        request[:id] = [666]
-
-        expect(subject.empty?).to be true
-      end
-
-      it 'fails when passed count' do
-        request[:count] = 5
-
-        expect { subject }.to raise_error(Nugator::Error::InvalidRequest)
-      end
-
-      it 'fails when passed before date' do
-        request[:before] = Time.now
-
-        expect { subject }.to raise_error(Nugator::Error::InvalidRequest)
-      end
-
-      it 'fails when passed since date' do
-        request[:since] = Time.now
-
-        expect { subject }.to raise_error(Nugator::Error::InvalidRequest)
-      end
-
-      it 'fails when passed tags' do
-        request[:tags] = %w[Test One]
-
-        expect { subject }.to raise_error(Nugator::Error::InvalidRequest)
-      end
+  context 'when not requesting content' do
+    it 'returns item without content' do
+      expect(subject.sample.content).to be nil
     end
   end
 
-  context 'when request for count' do
-    let(:id) { nil }
+  context 'when requesting content' do
+    let(:content) { true }
 
-    it 'returns items equal to count' do
-      request[:count] = 2
-
-      expect(subject.count).to be 2
-    end
-
-    it 'returns less items when found items are less than count' do
-      request[:count] = 2
-
-      expect(subject.count).to be 2
-    end
-
-    it 'does not return more items than count' do
-      request[:count] = 1
-
-      expect(subject.count).to be 1
-    end
-
-    context 'with invalid params' do
-      it 'fails when count is 0 or less' do
-        request[:count] = 0
-
-        expect { subject }.to raise_error Nugator::Error::InvalidRequest
-      end
-
-      it 'fails when count is more than 10' do
-        request[:count] = 11
-
-        expect { subject }.to raise_error Nugator::Error::InvalidRequest
-      end
-    end
-  end
-
-  context 'when request for items since datetime' do
-    let(:id) { nil }
-
-    it 'returns items' do
-      request[:since] = Time.parse('2000-01-01 00:00:00 UTC')
-
-      expect(subject.count).to be 11
-    end
-
-    it 'does not return items before date' do
-      request[:since] = Time.parse('2000-01-02 00:00:00 UTC')
-
-      item_dates = subject.map(&:published_date)
-
-      expect(item_dates.include?('2000-01-01 00:00:00 UTC')).to be false
-    end
-  end
-
-  context 'when request for items before datetime' do
-    let(:id) { nil }
-
-    it 'returns items' do
-      request[:before] = Time.parse('2000-01-02 00:00:00 UTC')
-
-      expect(subject.count).to be 2
-    end
-
-    it 'does not return items after date' do
-      request[:before] = Time.parse('2000-01-08 00:00:00 UTC')
-
-      item_dates = subject.map(&:published_date)
-
-      expect(item_dates.include?('2000-01-09 00:00:00 UTC')).to be false
-    end
-  end
-
-  context 'when request for tags' do
-    let(:id) { nil }
-
-    it 'returns items for tag' do
-      request[:tags] = %w[test_item]
-
-      expect(subject.count).to be 10
-    end
-
-    it 'returns items that has at least one tag in tags' do
-      request[:tags] = %w[test_tag_1]
-
-      expect(subject.count).to be 1
-    end
-
-    it 'does not return items without tag' do
-      request[:tags] = %w[test_tag_1]
-
-      expect(subject.count).to be 1
-    end
-  end
-
-  context 'when request is multiple lookups' do
-    let(:id) { nil }
-
-    it 'returns results for tags and since' do
-      request[:since] = Time.parse('2000-01-07 00:00:00 UTC')
-      request[:tags] = %w[test_item]
-
-      expect(subject.count).to be 4
-    end
-
-    it 'returns results for tags and before' do
-      request[:before] = Time.parse('2000-01-08 00:00:00 UTC')
-      request[:tags] = %w[test_tag_7]
-
-      expect(subject.count).to be 1
-    end
-
-    it 'returns results for tags and count' do
-      request[:tags] = %w[test_item]
-      request[:count] = 3
-
-      expect(subject.count).to be 3
-    end
-
-    it 'returns results for tags in date range' do
-      request[:since] = Time.parse('2000-01-04 00:00:00 UTC')
-      request[:before] = Time.parse('2000-01-08 00:00:00 UTC')
-      request[:tags] = %w[test_item test_tag_7]
-
-      expect(subject.count).to be 5
-    end
-
-    it 'returns results for date range' do
-      request[:since] = Time.parse('2000-01-04 00:00:00 UTC')
-      request[:before] = Time.parse('2000-01-08 00:00:00 UTC')
-
-      item_dates = subject.map(&:published_date)
-
-      expect(subject.count).to be 5
-      expect(item_dates.include?('2000-01-04 00:00:00 UTC')).to be true
-      expect(item_dates.include?('2000-01-06 00:00:00 UTC')).to be true
-      expect(item_dates.include?('2000-01-08 00:00:00 UTC')).to be true
-
-      expect(item_dates.include?('2000-01-03 00:00:00 UTC')).to be false
-      expect(item_dates.include?('2000-01-09 00:00:00 UTC')).to be false
-    end
-
-    it 'returns items if before and since are reversed' do
-      request[:before] = Time.parse('2000-01-04 00:00:00 UTC')
-      request[:since] = Time.parse('2000-01-08 00:00:00 UTC')
-
-      item_dates = subject.map(&:published_date)
-
-      expect(subject.count).to be 5
-      expect(item_dates.include?('2000-01-04 00:00:00 UTC')).to be true
-      expect(item_dates.include?('2000-01-06 00:00:00 UTC')).to be true
-      expect(item_dates.include?('2000-01-08 00:00:00 UTC')).to be true
-
-      expect(item_dates.include?('2000-01-03 00:00:00 UTC')).to be false
-      expect(item_dates.include?('2000-01-09 00:00:00 UTC')).to be false
-    end
-
-    it 'returns results for date range limited by count' do
-      request[:since] = Time.parse('2000-01-04 00:00:00 UTC')
-      request[:before] = Time.parse('2000-01-08 00:00:00 UTC')
-      request[:count] = 3
-
-      expect(subject.count).to be 3
+    it 'returns item with content' do
+      expect(subject.sample.content).to eq 'Test Content'
     end
   end
 end
