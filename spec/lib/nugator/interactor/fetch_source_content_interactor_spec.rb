35fd246e07dbce8b919bc9c20545f303686098ec
Gregory Daynes <gregdaynes@gmail.com>
2018-12-30 17:41:46 -0800

    Update fetch_content to use test persistance source
    Removes remote call, and speeds up tests by ~4x

--- spec/lib/nugator/interactor/fetch_source_content_interactor_spec.rb
+++ spec/lib/nugator/interactor/fetch_source_content_interactor_spec.rb
@@ -1,20 +1,27 @@
 require 'spec_helper'
 
 require 'dry/container/stub'
+require 'open-uri'
 
 require 'nugator'
 
 RSpec.describe Nugator::Interactor::FetchSourceContent do
-  let(:item_repository) { Persistance::Item.new }
+  let!(:item_repository) { Persistance::Item.new }
+  let!(:source_repository) { Persistance::Source.new }
 
   let(:instance) { described_class.new }
   let(:source) do
-    OpenStruct.new(url: 'http://www.golfchannel.com/rss/124552/feed.xml')
+    OpenStruct.new(url: 'https://example.com/feed.xml')
   end
 
   before do
     Nugator::Container.enable_stubs!
     Nugator::Container.stub('repository.item', item_repository)
+    Nugator::Container.stub('repository.source', source_repository)
+
+    # Stub out uri and file read for interactor test
+    allow(URI).to receive(:open) { Tempfile.new('temp_file') }
+    allow_any_instance_of(File).to receive(:read) { source_repository.source_content }
   end
 
   subject { instance.call(source) }
--- spec/lib/nugator/operation/source/fetch_content_spec.rb
+++ spec/lib/nugator/operation/source/fetch_content_spec.rb
@@ -1,24 +1,39 @@
 require 'spec_helper'
 
+require 'open-uri'
+
 require 'nugator'
 
 RSpec.describe Nugator::Operation::Source::FetchContent do
+  let!(:source_repository) { Persistance::Source.new }
+
   let(:instance) { described_class.new }
-  let(:url) { 'http://www.golfchannel.com/rss/124552/feed.xml' }
+  let(:url) { 'http://example.com/feed.xml' }
 
   subject { instance.call(url) }
 
   describe '#call' do
     context 'when source is available' do
+      before do
+        allow(URI).to receive(:open) { Tempfile.new('temp_file') }
+        allow_any_instance_of(File).to receive(:read) { source_repository.source_content }
+      end
+
       it 'fetches the source' do
         expect(subject).to be_a(Array)
       end
     end
 
     context 'when source is unavailable' do
+      it 'returns 404' do
+        expect { subject }.to raise_error(OpenURI::HTTPError, '404 Not Found')
+      end
+    end
+
+    context 'when source is unreadable' do
       let(:url) { 'http://example.com' }
 
-      it 'returns false' do
+      it 'returns 404' do
         expect { subject }.to raise_error(RSS::NotWellFormedError)
       end
     end
